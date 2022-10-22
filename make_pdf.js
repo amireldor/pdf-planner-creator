465f8a0670e5c6b136d032a5a8bf29309c5db85d
Amir Eldor <amir@eldor.dev>
2022-10-22 23:21:31 +0300

    Can make a nice planner, no daily yet

---
+++ make_pdf.js
@@ -0,0 +1,25 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+  await page.goto("http://localhost:3000");
+
+  const margin = {
+    top: "1cm",
+    bottom: "1cm",
+    left: "1.4cm",
+    right: "1.4cm",
+  };
+
+  const pdfConfig = {
+    path: "planner-2022.pdf",
+    format: "A5",
+    margin: margin,
+  };
+
+  await page.pdf(pdfConfig);
+
+  await browser.close();
+})();
